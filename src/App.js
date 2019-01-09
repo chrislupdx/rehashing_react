33c2139d5051469f309c8335767f3fd6496ca335
seeker <wildetangeant@gmail.com>
2019-01-08 17:45:56 -0800

    rendering content conditionally with ReactJS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { adj: 'bitter', number: 12 },
       { adj: 'lonely', number: 1 },
     ],
-    otherstate: 'some other value'
+    otherstate: 'some other value',
+    showPersons: false
   }
 
   boomHandler = (newThing) => {
@@ -31,6 +32,11 @@ class App extends Component {
       ] })
   })
 
+togglePersonHandler = () => {
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow});
+}
+
 
   render() {
     const style = {
@@ -46,18 +52,23 @@ class App extends Component {
         <p> i am fake web developer </p>
         <button
           style={style}
-          onClick={() => this.boomHandler('flooof')}> Push to Boom </button>
-        < Person
-          adj={this.state.persons[0].adj}
-          number={this.state.persons[0].number} />
-        < Person
-          adj={this.state.persons[1].adj}
-          number={this.state.persons[1].number}
-          click={this.boomHandler.bind(this, 'wrinkly')}
-          changed={this.nameChangeHandler} >I enjoy potatoes </Person>
-        <Person
-          adj={this.state.persons[2].adj}
-          number={this.state.persons[2].number} />
+          onClick={this.togglePersonHandler}> Toggle Statements </button>
+        {
+          this.state.showPersons === true ?
+          <div>
+          < Person
+            adj={this.state.persons[0].adj}
+            number={this.state.persons[0].number} />
+          < Person
+            adj={this.state.persons[1].adj}
+            number={this.state.persons[1].number}
+            click={this.boomHandler.bind(this, 'wrinkly')}
+            changed={this.nameChangeHandler} >I enjoy potatoes </Person>
+          <Person
+            adj={this.state.persons[2].adj}
+            number={this.state.persons[2].number} />
+        </div> : null
+      }
       </div>
     );
   // return React.createElement('div', null, React.createElement('h3', {className: 'App'}, 'fools rush on'));
