2d364a6fb72185012b6aca457fe88b9a6f81a5ea
seeker <wildetangeant@gmail.com>
2019-01-08 17:09:55 -0800

    Playing with css styling

--- src/App.js
+++ src/App.js
@@ -7,30 +7,46 @@ class App extends Component {
     persons: [
       { adj: 'spiteful', number: 1000000000000 },
       { adj: 'bitter', number: 12 },
-      { adj: 'lonely', number: 0 },
+      { adj: 'lonely', number: 1 },
     ],
     otherstate: 'some other value'
   }
 
-  boomHandler = () => {
+  boomHandler = (newThing) => {
     // console.log('Was clicked');
     this.setState({
       persons: [
-      { adj: 'hangry', number: 1000000000000 },
+      { adj: newThing, number: 1000000000000 },
       { adj: 'bitter', number: 12 },
-      { adj: 'lonely', number: 1 },
+      { adj: 'lonely', number: 11111111 },
       ] })
   }
 
+  nameChangeHandler = (event => {
+    this.setState({
+      persons: [
+      { adj: 'Gross', number: 1000000000000 },
+      { adj: event.target.value, number: 12 },
+      { adj: 'lonely', number: 11111111 },
+      ] })
+  })
   render() {
     return (
       <div className="App">
         <h1>am react app</h1>
         <p> i am fake web developer </p>
-        <button onClick={this.boomHandler}> Push to Boom </button>
-        < Person adj={this.state.persons[0].adj} number={this.state.persons[0].number} />
-        < Person adj={this.state.persons[1].adj} number="1333333" />
-        < Person adj={this.state.persons[2].adj} number="2" /> I do however really enjoy romance novels <Person/>
+        <button onClick={() => this.boomHandler('flooof')}> Push to Boom </button>
+        < Person
+          adj={this.state.persons[0].adj}
+          number={this.state.persons[0].number} />
+        < Person
+          adj={this.state.persons[1].adj}
+          number={this.state.persons[1].number}
+          click={this.boomHandler.bind(this, 'wrinkly')}
+          changed={this.nameChangeHandler} >I enjoy potatoes </Person>
+        <Person
+          adj={this.state.persons[2].adj}
+          number={this.state.persons[2].number} />
       </div>
     );
   // return React.createElement('div', null, React.createElement('h3', {className: 'App'}, 'fools rush on'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = ( props ) => {
   return (
-    <div>
-  <p>Mortality is a constant. I am {props.adj} and I own {props.number} cats...</p>
-  <p>{props.children}</p>
+    <div className = "Person">
+      <p onClick={props.click}>Mortality is a constant. I am {props.adj} and I own {props.number} cats...</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value = {props.name} />
     </div>
 )
 };
